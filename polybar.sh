d3c21216ac81f78ee4688e825715e3ca9f68107c
skAdOOdlEs7 <grantdriddle7@gmail.com>
2022-09-08 16:39:46 -0600

    tweaked polybar layout

--- polybar.sh
+++ polybar.sh
@@ -1,4 +1,4 @@
-#yay -S --noconfirm polybar ttf-jetbrains-mono ttf-font-awesome ttf-material-design-icons-webfont python-pip 
-#pip install dbus-python
+yay -S --noconfirm polybar ttf-jetbrains-mono ttf-font-awesome ttf-material-design-icons-webfont python-pip 
+pip install dbus-python
 
 ln -sf $PWD/config_files/polybar /$HOME/.config/
