94fb94df974fbd1c8b27d8e2bfe30224ddc13beb
skAdOOdlEs7 <grantdriddle7@gmail.com>
2022-09-08 16:40:13 -0600

    changed terminal theme

--- terminal.sh
+++ terminal.sh
@@ -2,6 +2,7 @@ sudo pacman -S --noconfirm zsh alacritty neovim
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ln -sf $PWD/config_files/terminal/.zshrc /home/grant
+ln -sf $PWD/config_files/terminal/donger.zsh-theme ~/.oh-my-zsh/themes
 
 mkdir ~/.config/alacritty
 ln -sf $PWD/config_files/terminal/alacritty.yml ~/.config/alacritty/alacritty.yml
